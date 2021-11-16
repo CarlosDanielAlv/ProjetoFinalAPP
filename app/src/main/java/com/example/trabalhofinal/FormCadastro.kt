096b6581138337e013a429663ce7be68d6623a3b
CarlosDanielAlv <carlosdalves.ads@gmail.com>
2021-11-16 16:04:04 -0300

    Implementação de ações de navegação e alteração de nome ID botões.

--- app/src/main/java/com/example/trabalhofinal/FormCadastro.kt
+++ app/src/main/java/com/example/trabalhofinal/FormCadastro.kt
@@ -1,12 +1,22 @@
 package com.example.trabalhofinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FormCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_cadastro)
+
+        // Botão Confirmar
+        val button = findViewById<Button>(R.id.bt_confirmar)
+        // Ação Botão Confirmar
+        button.setOnClickListener(){
+            val intent = Intent(this, TelaPerfil::class.java)
+            startActivity(intent)
+        }
 
         // Escondendo barra de ação da tela
         supportActionBar?.hide()
--- app/src/main/java/com/example/trabalhofinal/TelaPerfil.kt
+++ app/src/main/java/com/example/trabalhofinal/TelaPerfil.kt
@@ -1,7 +1,9 @@
 package com.example.trabalhofinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TelaPerfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,14 @@ class TelaPerfil : AppCompatActivity() {
 
         // Escondendo barra de ação da tela
         supportActionBar?.hide()
+
+        // Botão Deslogar
+        val buttonDeslogar = findViewById<Button>(R.id.bt_deslogar)
+
+        buttonDeslogar.setOnClickListener(){
+            val intent = Intent(this, FormLogin::class.java)
+            startActivity(intent)
+        }
 
     }
 }
