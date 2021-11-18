ada99d8ff415e2d844644a00b9a6b8f0142d77f6
CarlosDanielAlv <carlosdalves.ads@gmail.com>
2021-11-17 21:13:45 -0300

    Subindo telas de navegação

--- app/src/main/java/com/example/trabalhofinal/MainActivity.kt
+++ app/src/main/java/com/example/trabalhofinal/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.trabalhofinal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Escondendo barra de ação da tela
+        supportActionBar?.hide()
+
+        // Ação do Navegation --
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment)
+        val navController = navHostFragment?.findNavController()
+
+        // Validação para verificar se é diferente de null o click
+        if (navController != null) {
+            bottomNavigationView.setupWithNavController(navController)
+        }
+
+
     }
 }
